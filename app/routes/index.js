75d3f30feedda864ae4756bf6c535e5e46004cca
erik tolentino <tolentino3575@gmail.com>
2016-04-01 09:12:27 -0700

    Add questions.json, attach firebase

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
+// var questions = [{
+//   id: 1,
+//   author: "Erik P.",
+//   content: "What should I get for lunch?",
+//   notes: "Pls respond quick! I'm hungry!"
+// }, {
+//   id: 2,
+//   author: "Erik C.",
+//   content: "What's the temperaure of the sun?",
+//   notes: "It's really bright today and I'm sweating more than usual..",
+// }, {
+//   id: 3,
+//   author: "Erik B.",
+//   content: "Should I be concerned about the mole on my toe?",
+//   notes: "It's huge. Like half dollar big."
+// }];
+
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
