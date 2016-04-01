998c00cdc5f6a2fe41f104ed775d3c7b67ac715f
erik tolentino <tolentino3575@gmail.com>
2016-04-01 11:18:19 -0700

    Try to work model relationship

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   author: DS.attr(),
   title: DS.attr(),
   content: DS.attr(),
-  notes: DS.attr()
+  notes: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+
+  actions: {
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+    }
+  }
 });
