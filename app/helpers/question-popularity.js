3597c1d87d62359ffa2bfcc34f1af8ac9049df08
erik tolentino <tolentino3575@gmail.com>
2016-04-08 09:10:09 -0700

    Add custom helper to display thumbs up, thumbs down, thumbs up/fire depending on amount of answers

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export function questionPopularity(params/*, hash*/) {
+  var question = params[0];
+
+  if(question.get('answers').get('length') >= 5) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span> <span class="glyphicon glyphicon-thumbs-up"></span>');
+  } else if((question.get('answers').get('length') < 5) && (question.get('answers').get('length') >= 3)) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+  } else if(question.get('answers').get('length') < 3) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-thumbs-down"></span');
+  }
+
+}
+export default Ember.Helper.helper(questionPopularity);
